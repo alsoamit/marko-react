db8904b857c802dd6a86a14f8816ff5dbbfab4e7
Amit Kumar <alsoamit@gmail.com>
2021-09-14 14:23:24 +0530

    resizer added

--- src/App.js
+++ src/App.js
@@ -5,17 +5,35 @@ function App() {
   const [rawData, setRawData] = useState(
     `# Markdown Editor\nConverts your Markdown into HTML`
   );
+  const [width, setWidth] = useState("50%");
 
   function updater(e) {
     setRawData(e.target.value);
   }
+
   return (
     <div className="App">
-      <textarea
-        value={rawData}
-        className="editor"
-        onChange={updater}
-      ></textarea>
+      {/* the text area */}
+      <div style={{ width: width }} className="editor">
+        <textarea value={rawData} onChange={updater}></textarea>
+      </div>
+
+      {/* the resizer */}
+      <div className="resizer-wrap">
+        <div
+          draggable
+          onDrag={(e) => {
+            if (e.clientX) {
+              setWidth(e.clientX.toString() + "px");
+              console.log(e.clientX.toString() + "px");
+            }
+            console.log(e.clientX.toString() + "px");
+          }}
+          className="resizer"
+        ></div>
+      </div>
+
+      {/* the previewer */}
       <div
         className="previewer"
         dangerouslySetInnerHTML={{ __html: marked(rawData) }}
