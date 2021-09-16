1d667bd4190cd51f8f75f87b4c114b79e99407ce
Amit Kumar <alsoamit@gmail.com>
2021-09-16 10:23:21 +0530

    Added link for the logo.

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ function Navbar() {
     <header>
       <nav>
         <div className="logo__wrap">
-          <img src="/" alt=""></img>
+          <img src="https://github.com/alsoamit/marko-react/blob/master/public/logo.png" alt="marko-logo"></img>
           <h1>Marko</h1>
         </div>
         <button className="btn">New File</button>
